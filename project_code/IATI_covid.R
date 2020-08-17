5d8cbfd662ae9667ec6234870531f308774b892d
danjwalton <20973540+danjwalton@users.noreply.github.com>
2020-08-17 10:50:01 +0100

    Update to IATI data

--- project_code/IATI_covid.R
+++ project_code/IATI_covid.R
@@ -3,7 +3,7 @@ lapply(required.packages, require, character.only=T)
 
 setwd("G:/My Drive/Work/GitHub/nexus_countries/")
 
-get.query.data <- function(filename=NULL, query_id=NULL, path="project_data", force.update = F, remove.old = T){
+get.query.data <- function(filename=NULL, query_id=NULL, path="project_data", force.update = F, remove.old = F){
   if(is.null(filename) & is.null(query_id)) stop("Please specify a filename and/or query id.", call.=F)
   query <- paste0("https://ddw.devinit.org/api/export/", query_id, "/")
   old.files <- list.files(path, paste0(filename, ".*csv"))
@@ -21,25 +21,32 @@ get.query.data <- function(filename=NULL, query_id=NULL, path="project_data", fo
   return(query.data)
 }
 
-iati.raw <- get.query.data("Nexus_IATI", 638, force.update = F)
+iati.raw <- get.query.data("Nexus_IATI", 638, force.update = F, remove.old = T)
 
-iati <- iati.raw[x_default_vocabulary != ""]
-iati <- iati[x_transaction_year >= 2016]
+iati <- iati.raw[`Calculated Default Vocabulary` != ""]
+iati <- iati[`Calculated Transaction Year` >= 2016]
 
-iati$x_transaction_month_year <- format(as.Date(iati$x_transaction_date), "%Y-%m")
-iati$x_covid_corrected <- ifelse(iati$x_transaction_year < 2020, FALSE, iati$x_covid)
+iati$x_transaction_month_year <- format(as.Date(iati$`Calculated Transaction Date`), "%Y-%m")
+iati$x_covid_corrected <- ifelse(as.Date(iati$`Calculated Transaction Date`) < as.Date("2020-03-01"), FALSE, iati$`X Covid flag`)
 
-iati <- iati[as.Date(x_transaction_date) < as.Date("2020-7-1")]
+iati$humanitarian_corrected <- ifelse(iati$Humanitarian == "true" | iati$Humanitarian == "1", "humanitarian", "development")
 
-nexus.covid <- dcast(as.data.table(iati), x_country + x_covid_corrected ~ x_transaction_month_year, value.var="x_transaction_value_usd", fun.aggregate = function(x) sum(x, na.rm=T))
-nexus.covid <- dcast(melt(nexus.covid, id.vars = c("x_country", "x_covid_corrected")), x_country + variable ~ x_covid_corrected, value.var="value")
+iati <- iati[as.Date(`Calculated Transaction Date`) < as.Date("2020-8-1")]
 
-nexus.covid[, c("FALSE", "seasonal", "trend", "random") := decompose(ts(`FALSE`, frequency = 12), "multiplicative"), by=x_country]
-nexus.covid[, FALSE_adjusted := `FALSE`/seasonal, by=x_country]
+nexus.covid <- dcast(as.data.table(iati), Country + x_covid_corrected + humanitarian_corrected ~ x_transaction_month_year, value.var="Calulated Transaction Value USD", fun.aggregate = function(x) sum(x, na.rm=T))
+nexus.covid <- dcast(melt(nexus.covid, id.vars = c("Country", "x_covid_corrected", "humanitarian_corrected")), Country + variable ~ x_covid_corrected + humanitarian_corrected, value.var="value")
 
-ggplot(nexus.covid, aes(y = FALSE_adjusted, x = as.numeric(variable)))+
-  geom_line(aes(color=x_country))
+nexus.covid[, `:=` (FALSE_total = FALSE_humanitarian + FALSE_development, TRUE_total = TRUE_humanitarian + TRUE_development)]
 
-list2env(split(nexus.covid, nexus.covid$x_country), envir = .GlobalEnv)
+nexus.covid[, c("FALSE_total", "seasonal", "trend", "random") := decompose(ts(FALSE_total, frequency = 12), "multiplicative"), by=Country]
+nexus.covid[, FALSE_total_adjusted := FALSE_total/seasonal, by=Country]
+
+ggplot(nexus.covid, aes(y = FALSE_total_adjusted, x = as.numeric(variable)))+
+  geom_line(aes(color=Country))
+
+list2env(split(nexus.covid, nexus.covid$Country), envir = .GlobalEnv)
+
+nexus.covid$year <- substr(nexus.covid$variable, 0, 4)
+nexus.covid$month <- month.abb[as.numeric(substr(nexus.covid$variable, 6, 8))]
 
 fwrite(nexus.covid, "output/covid_trend.csv")
